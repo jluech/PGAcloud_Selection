76d35dfc988eb7f464a31c6801032874a8df3b4b
jluech <janik@luechinger.ch>
2020-07-20 12:24:55 +0200

    feat: implement roulette wheel selection
    refs #106

--- selection/selectors.py
+++ selection/selectors.py
@@ -1,3 +1,5 @@
+import math
+import random
 from abc import ABC, abstractmethod
 
 
@@ -11,7 +13,32 @@ class AbstractSelection(ABC):
 
 class RouletteWheelSelection(AbstractSelection):
     def perform_selection(self, population):
-        pass
+        size = population.__len__()
+        iterations = math.ceil(size / 2)
+        parents = []
+
+        fitness_sum = 0
+        for individual in population:
+            fitness_sum += individual.get("fitness")
+
+        for i in range(iterations):
+            pair = Pair()
+            pair.p1 = self.__spin_the_wheel(population, fitness_sum)
+            pair.p2 = self.__spin_the_wheel(population, fitness_sum)
+            parents.append(pair)
+
+        return parents
+
+    def __spin_the_wheel(self, population, fitness_sum):
+        partial_fitness_sum = 0
+        wheel_spin = random.randint(0, fitness_sum)  # select individual at random
+        selected = population[0]  # select first individual if wheel_spin=0
+        for individual in population:  # retrieve selected individual
+            if partial_fitness_sum >= wheel_spin:  # consider fitness boundaries can also be selected
+                break
+            selected = individual
+            partial_fitness_sum += individual.get("fitness")
+        return selected
 
 
 class TournamentSelection(AbstractSelection):
