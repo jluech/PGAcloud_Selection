496648b9ecdeb94fb38c0c7e76390124c772fc43
jluech <janik@luechinger.ch>
2020-07-22 16:26:31 +0200

    feat: retrieve PGA ID from config file
    refs #109

--- utilities/utils.py
+++ utilities/utils.py
@@ -1,6 +1,12 @@
+import logging
+import os
 from enum import Enum
+from re import match
+
+import yaml
 
 __SELECTOR = None
+__PGA_ID = None
 
 
 class MessageHandlers(Enum):
@@ -11,6 +17,33 @@ class Selectors(Enum):
     RouletteWheel = "roulette",
     Tournament = "tournament",
     Rank = "rank",
+
+
+def parse_yaml(yaml_file_path):
+    with open(yaml_file_path, mode="r", encoding="utf-8") as yaml_file:
+        content = yaml.safe_load(yaml_file) or {}
+    return content
+
+
+def get_pga_id():
+    if not __PGA_ID:
+        return __retrieve_pga_id()
+    return __PGA_ID
+
+
+def __retrieve_pga_id():
+    # Retrieve locally saved config file.
+    files = [f for f in os.listdir("/") if match(r'[0-9]+--config\.yml', f)]
+    # https://stackoverflow.com/questions/2225564/get-a-filtered-list-of-files-in-a-directory/2225927#2225927
+    # https://regex101.com/
+
+    if not files.__len__() > 0:
+        raise Exception("Error retrieving the PGA ID: No matching config file found!")
+    config = parse_yaml("/{}".format(files[0]))
+    global __PGA_ID
+    __PGA_ID = config.get("pga_id")
+    logging.info("PGA_ID {id_} retrieved.".format(id_=__PGA_ID))
+    return __PGA_ID
 
 
 def forward_selector():
